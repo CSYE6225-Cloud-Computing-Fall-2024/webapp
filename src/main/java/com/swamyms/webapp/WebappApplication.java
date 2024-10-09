46f43bc219bda5a3c5768a085e6f71acfc4167d9
swamyms <mudiga.s@northeastern.edu>
2024-10-09 14:54:21 -0400

    Added Spring DotEnv Dependencies therefore removed load environment vvariables as Spring will automatically load the .env

--- src/main/java/com/swamyms/webapp/WebappApplication.java
+++ src/main/java/com/swamyms/webapp/WebappApplication.java
@@ -1,6 +1,6 @@
 package com.swamyms.webapp;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebappApplication {
 
 	public static void main(String[] args) {
-
-		// Load environment variables from .env file
-		Dotenv dotenv = Dotenv.load();
-
-		// Set system properties to be used in application.properties
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(WebappApplication.class, args);
 	}
 
