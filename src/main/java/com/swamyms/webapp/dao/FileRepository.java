61b4da94b2465bec535099faab477fb035fa72f0
swamyms <mudiga.s@northeastern.edu>
2024-10-27 23:33:02 -0400

    Added ImageExists logic by using User ID as input Implementing findImageDetailsByUserID

--- src/main/java/com/swamyms/webapp/dao/FileRepository.java
+++ src/main/java/com/swamyms/webapp/dao/FileRepository.java
@@ -2,8 +2,16 @@
 
     import com.swamyms.webapp.entity.file.FileEntity;
     import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.jpa.repository.Query;
     import org.springframework.stereotype.Repository;
+
+    import java.util.Optional;
 
     @Repository
     public interface FileRepository extends JpaRepository<FileEntity, String> {
+        boolean existsByUserId(String userID);
+
+        @Query("SELECT f FROM FileEntity f WHERE f.user.id = :userId")
+        FileEntity findByUser_Id(String userId);
+
     }
