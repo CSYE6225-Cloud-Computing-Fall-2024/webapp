816d47deb400cb25c1f0ece739eb58d3744b26ea
swamyms <mudiga.s@northeastern.edu>
2024-10-28 19:52:32 -0400

    1. Added GetImageDetails and DeleteImageDetails based on the UserID 2. Storing the Image details in S3 bucket using s3Config 3. Using userID as key for S3 bucket which will be used to store and delete teh image.

--- src/main/java/com/swamyms/webapp/service/FileService.java
+++ src/main/java/com/swamyms/webapp/service/FileService.java
@@ -2,20 +2,21 @@
 
     import com.swamyms.webapp.dao.FileRepository;
     import com.swamyms.webapp.entity.User;
-    import com.swamyms.webapp.entity.file.FileEntity;
     import com.swamyms.webapp.entity.file.FileMapper;
     import com.swamyms.webapp.entity.file.model.FileUploadRequest;
     import com.swamyms.webapp.entity.file.model.FileUploadResponse;
+    import com.swamyms.webapp.exceptionhandling.exceptions.ResourceNotFoundException;
+    import org.springframework.beans.factory.annotation.Value;
     import org.springframework.stereotype.Service;
     import org.springframework.web.multipart.MultipartFile;
     import software.amazon.awssdk.core.sync.RequestBody;
     import software.amazon.awssdk.services.s3.S3Client;
+    import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
     import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
     import java.io.IOException;
-    import java.nio.file.Files;
-    import java.nio.file.Path;
-    import java.nio.file.Paths;
+    import java.net.URLEncoder;
+    import java.nio.charset.StandardCharsets;
 
 
     @Service
@@ -25,6 +26,8 @@
         private final FileMapper fileMapper;
         private final S3Client s3Client;
 
+        @Value("${aws.s3.bucket.name}")
+        private String bucketName;
 
         public FileService(FileRepository theFileRepository, FileMapper theFileMapper, S3Client theS3Client){
             this.fileRepository = theFileRepository;
@@ -35,41 +38,33 @@
         public FileUploadResponse upload(FileUploadRequest fileUploadRequest, User user) throws IOException {
 
             MultipartFile file = fileUploadRequest.file();
-            String fileName = user.getId() + "/" + file.getOriginalFilename(); // Customize path as needed
-
-            // Define the S3 bucket name
-            String bucketName = "SwamyMudigaS3Bucket"; // Replace with your actual bucket name
-
-
+            String rawFileName = file.getOriginalFilename();
+            String fileName = user.getId() + "/" + rawFileName;
 
             try{
-//                MultipartFile file = fileUploadRequest.file();
-//
-//                // Define file location
-//                String fileName = user.getId() + "/" + file.getOriginalFilename();
 
                 s3Client.putObject(PutObjectRequest.builder()
                                 .bucket(bucketName)
-                                .key(fileName)
+                                .key(user.getId())
                                 .build(),
                         RequestBody.fromBytes(file.getBytes()));
 
-                // Optionally, you can store the S3 URL for future reference
-                String s3Url = "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
+                // Encode the file name for URL use
+                String encodedFileName = URLEncoder.encode(rawFileName, StandardCharsets.UTF_8.toString());
+                String s3Url = "https://" + bucketName + ".s3.amazonaws.com/" + user.getId() + "/" + encodedFileName;
 
-//                String url = bucketName + fileName;
 
-                // Save file to local storage or server (can replace this with cloud storage logic)
-                Path filePath = Paths.get("uploads/" + fileName);
-                Files.createDirectories(filePath.getParent());
-                Files.write(filePath, file.getBytes());
+                // Save file to local storage
+//                Path filePath = Paths.get("uploads/" + fileName);
+//                Files.createDirectories(filePath.getParent());
+//                Files.write(filePath, file.getBytes());
 //                String filePathString = filePath.toString();
+
                 var fileEntity = fileMapper.toEntity(newID(), fileUploadRequest, user, s3Url);
                 fileRepository.save(fileEntity);
 
                 return fileMapper.toFileUploadResponse(fileEntity);
             }catch (Exception e) {
-                // Handle exceptions, e.g., log the error and throw a custom exception if needed
                 throw new RuntimeException("File upload failed: " + e.getMessage());
             }
         }
@@ -79,10 +74,26 @@
             return fileRepository.existsByUserId(userID);
         }
 
-        public FileEntity getImageDetailsByUserID(String userID){
-            return fileRepository.findByUser_Id(userID);
+        public FileUploadResponse getImageDetailsByUserID(String userID){
+            return fileMapper.toFileUploadResponse(fileRepository.findByUser_Id(userID));
         }
-//        User getUserByEmail(String email);
+
+        public void deleteImageDetailsByUserID(String userID){
+            // Check if the file exists
+            if (!fileRepository.existsByUserId(userID)) {
+                throw new ResourceNotFoundException();
+            }
+
+            // Delete the object from S3
+            s3Client.deleteObject(DeleteObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(userID)
+                    .build());
+            // If it exists, delete the user picture
+            fileRepository.deleteByUserId(userID);
+//            fileRepository.deleteById(userID);
+//            return fileMapper.toFileUploadResponse(fileRepository.findByUser_Id(userID));
+        }
 
 
         private String newID(){
