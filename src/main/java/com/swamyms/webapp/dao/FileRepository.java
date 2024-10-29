63ac88e2709bb56dd53a7329a5fbf11e889bd989
swamyms <mudiga.s@northeastern.edu>
2024-10-28 23:02:36 -0400

    Added findFileNameByUserId() and deleteByUserId()

--- src/main/java/com/swamyms/webapp/dao/FileRepository.java
+++ src/main/java/com/swamyms/webapp/dao/FileRepository.java
@@ -1,11 +1,11 @@
     package com.swamyms.webapp.dao;
 
     import com.swamyms.webapp.entity.file.FileEntity;
+    import jakarta.transaction.Transactional;
     import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.jpa.repository.Modifying;
     import org.springframework.data.jpa.repository.Query;
     import org.springframework.stereotype.Repository;
-
-    import java.util.Optional;
 
     @Repository
     public interface FileRepository extends JpaRepository<FileEntity, String> {
@@ -13,5 +13,14 @@
 
         @Query("SELECT f FROM FileEntity f WHERE f.user.id = :userId")
         FileEntity findByUser_Id(String userId);
+
+        @Query("SELECT f.fileName FROM FileEntity f WHERE f.user.id = :userId")
+        String findFileNameByUserId(String userId);
+
+        // Add a delete method for user ID
+        @Modifying
+        @Transactional
+        @Query("DELETE FROM FileEntity f WHERE f.user.id = :userId")
+        void deleteByUserId(String userId);
 
     }
