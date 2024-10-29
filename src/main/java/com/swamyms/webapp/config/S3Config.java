3fb4e126a632fc7bbdabaebc9d496fb65e08f236
swamyms <mudiga.s@northeastern.edu>
2024-10-28 23:06:23 -0400

    awsProfileName and awsRegion made dynamic value

--- src/main/java/com/swamyms/webapp/config/S3Config.java
+++ src/main/java/com/swamyms/webapp/config/S3Config.java
@@ -1,5 +1,6 @@
 package com.swamyms.webapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
@@ -8,12 +9,18 @@ import software.amazon.awssdk.regions.Region;
 
 @Configuration
 public class S3Config {
+    @Value("${aws.profile.name}")
+    private String awsProfileName;
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .credentialsProvider(ProfileCredentialsProvider.create("dev")) // Use your profile name here
-                .region(Region.US_EAST_1) // Change this to your desired region
+                .credentialsProvider(ProfileCredentialsProvider.create(awsProfileName)) // Passing the profile name from .env
+                .region(Region.of(awsRegion)) // Passing the desired region from .env
                 .build();
     }
 }
