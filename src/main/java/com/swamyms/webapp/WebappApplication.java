e03dfef302e6dd34ba70443657bf064878ec89b5
swamyms <mudiga.s@northeastern.edu>
2024-10-30 19:25:03 -0400

    Added timedAspect to set the @Timed when the controller class starts

--- src/main/java/com/swamyms/webapp/WebappApplication.java
+++ src/main/java/com/swamyms/webapp/WebappApplication.java
@@ -1,12 +1,19 @@
 package com.swamyms.webapp;
 
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebappApplication {
 
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WebappApplication.class, args);
 	}
