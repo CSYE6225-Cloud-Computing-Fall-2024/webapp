4dd1bcfb9c26c682d86743b47e28fe504a23846c
swamyms <mudiga.s@northeastern.edu>
2024-10-31 11:58:10 -0400

    Logger and metrics added

--- src/main/java/com/swamyms/webapp/controllers/FileController.java
+++ src/main/java/com/swamyms/webapp/controllers/FileController.java
@@ -2,9 +2,14 @@ package com.swamyms.webapp.controllers;
 
 import com.swamyms.webapp.entity.User;
 import com.swamyms.webapp.entity.file.model.FileUploadRequest;
+import com.swamyms.webapp.entity.file.model.FileUploadResponse;
 import com.swamyms.webapp.service.FileService;
 import com.swamyms.webapp.service.UserService;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.validation.annotation.Validated;
@@ -19,15 +24,24 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("v1/user/self/pic")
 public class FileController {
+    private static final Logger logger = LoggerFactory.getLogger(HealthzRestController.class);
 
     private FileService fileService;
     private UserService userService;
+    private MeterRegistry meterRegistry;
+    private final Timer apiCallTimer;
 
     @Autowired
-    public FileController(FileService theFileService, UserService theUserService) {
+    public FileController(FileService theFileService, UserService theUserService, MeterRegistry meterRegistry) {
 
         this.fileService = theFileService;
         this.userService = theUserService;
+        this.meterRegistry = meterRegistry;
+
+        // Register your metrics here
+        this.apiCallTimer = Timer.builder("api.image.calls")
+                .description("Time taken for user check API calls")
+                .register(meterRegistry);
     }
 
     @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -38,26 +52,32 @@ public class FileController {
                                                                 ) {
 
         try{
+            long startTime = System.currentTimeMillis(); // Start timing API call
+            Timer.Sample sample = Timer.start(meterRegistry); // Start Timer for the API call
 
             if (params != null && !params.isEmpty()) {
+                logger.error("Bad request: unexpected parameters");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
             }
             String requestContentType = request.getContentType();
             if (requestContentType == null || !requestContentType.startsWith("multipart/form-data")) {
+                logger.error("Bad request: unexpected content type");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
 //                        .body("Only 'multipart/form-data' content type is allowed");
             }
             if (requestBody != null && !requestBody.isEmpty()) {
+                logger.error("Bad request: unexpected body present");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
             }
 
             if (file==null || file.getOriginalFilename().isEmpty()) {
+                logger.error("Bad request: file not present");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).body("GetMapping User error: Params are present or body is not null");
             }
             // Validate file type
             String contentType = file.getContentType();
             if (!isSupportedContentType(contentType)) {
+                logger.error("Bad request: invalid content type");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
 //                        .body("Bad Request: Unsupported file type. Only PNG, JPG, and JPEG files are allowed.");
             }
@@ -66,11 +86,13 @@ public class FileController {
 
         //if user provides only username or password, or does not provides any credential, return bad request
         if (userCreds.length < 2 || userCreds[0].isEmpty() || userCreds[1].isEmpty()) {
+            logger.error("Bad request: invalid user credentials");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
         }
 
         boolean checkUserPassword = userService.authenticateUser(userCreds[0], userCreds[1]);
         if (!checkUserPassword) {
+            logger.error("Unauthorized request: invalid user credentials");
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).cacheControl(CacheControl.noCache()).build();
         }
 
@@ -81,8 +103,13 @@ public class FileController {
 
        if(!imageExists){
            FileUploadRequest fileUploadRequest = new FileUploadRequest(file);
-           return ResponseEntity.ok(fileService.upload(fileUploadRequest, user));
+           FileUploadResponse fileUploadResponse = fileService.upload(fileUploadRequest, user);
+           logger.info("Image uploaded successfully. Time taken: {} ms", System.currentTimeMillis() - startTime); // Log info
+           sample.stop(apiCallTimer);
+           return ResponseEntity.ok(fileUploadResponse);
+
        }else{
+           logger.error("Bad request: Image already exists for user");
            return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
 //                   .body("Profile pic already exists for User: " + user.getEmail());
        }
@@ -93,7 +120,7 @@ public class FileController {
 //        FileUploadRequest fileUploadRequest = new FileUploadRequest(file, name);
 //        return ResponseEntity.ok(fileService.upload(fileUploadRequest, user));
         }catch (Exception e){
-            e.printStackTrace();
+            logger.error("Internal Server error Post Request /user/self/pic: Exception connection : {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).cacheControl(CacheControl.noCache()).build();
         }
     }
@@ -105,11 +132,15 @@ public class FileController {
                                                 @RequestBody(required = false) String requestBody)
     {
         try{
+            long startTime = System.currentTimeMillis(); // Start timing API call
+            Timer.Sample sample = Timer.start(meterRegistry); // Start Timer for the API call
             if (params != null && !params.isEmpty()) {
+                logger.error("Bad request: unexpected parameters");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
             }
 
             if (requestBody != null && !requestBody.isEmpty()) {
+                logger.error("Bad request: unexpected body");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
             }
 
@@ -118,11 +149,13 @@ public class FileController {
 
             //if user provides only username or password, or does not provides any credential, return bad request
             if (userCreds.length < 2 || userCreds[0].isEmpty() || userCreds[1].isEmpty()) {
+                logger.error("Bad request: invalid user credentials");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
             }
 
             boolean checkUserPassword = userService.authenticateUser(userCreds[0], userCreds[1]);
             if (!checkUserPassword) {
+                logger.error("Unauthorized request: invalid user credentials");
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).cacheControl(CacheControl.noCache()).build();
             }
 
@@ -133,13 +166,17 @@ public class FileController {
                     getUserImageByUserID(user.getId());
 
             if(imageExists){
-                return ResponseEntity.ok(fileService.getImageDetailsByUserID(user.getId()));
+                FileUploadResponse fileUploadResponse = fileService.getImageDetailsByUserID(user.getId());
+                logger.info("Image fetched successfully. Time taken: {} ms", System.currentTimeMillis() - startTime); // Log info
+                sample.stop(apiCallTimer);
+                return ResponseEntity.ok(fileUploadResponse);
             }else{
+                logger.error("Not request: Image does not exists for the user");
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).cacheControl(CacheControl.noCache()).build();
 //                        .body("Profile pic doesn't exists for User: " + user.getEmail());
             }
         }catch (Exception e){
-            e.printStackTrace();
+            logger.error("Internal Server error Get Request /user/self/pic: Exception connection : {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).cacheControl(CacheControl.noCache()).build();
         }
     }
@@ -158,11 +195,17 @@ public class FileController {
                                                 @RequestBody(required = false) String requestBody)
     {
         try{
+
+            long startTime = System.currentTimeMillis(); // Start timing API call
+            Timer.Sample sample = Timer.start(meterRegistry); // Start Timer for the API call
+
             if (params != null && !params.isEmpty()) {
+                logger.error("Bad request: unexpected parameters");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
             }
 
             if (requestBody != null && !requestBody.isEmpty()) {
+                logger.error("Bad request: unexpected request body");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
             }
 
@@ -171,11 +214,13 @@ public class FileController {
 
             //if user provides only username or password, or does not provides any credential, return bad request
             if (userCreds.length < 2 || userCreds[0].isEmpty() || userCreds[1].isEmpty()) {
+                logger.error("Bad request: user invalid credentials");
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).cacheControl(CacheControl.noCache()).build();
             }
 
             boolean checkUserPassword = userService.authenticateUser(userCreds[0], userCreds[1]);
             if (!checkUserPassword) {
+                logger.error("Unauthorized request: user invalid credentials");
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).cacheControl(CacheControl.noCache()).build();
             }
 
@@ -187,8 +232,11 @@ public class FileController {
 
             if(imageExists){
                 fileService.deleteImageDetailsByUserID(user.getId());
+                logger.info("Image Deleted successfully. Time taken: {} ms", System.currentTimeMillis() - startTime); // Log info
+                sample.stop(apiCallTimer);
                 return ResponseEntity.noContent().build();
             }else{
+                logger.error("Not Found request: Image doesn't exist to do PUT request /user/self/pic");
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).cacheControl(CacheControl.noCache()).build();
 //                        .body("Profile pic doesn't exists therefore can't be deleted for User: " + user.getEmail());
             }
@@ -210,6 +258,7 @@ public class FileController {
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl("no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
+        logger.error("PUT /user/self/pic called, returning METHOD_NOT_ALLOWED.");
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
     }
 
@@ -218,6 +267,7 @@ public class FileController {
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl("no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
+        logger.error("Patch /user/self/pic called, returning METHOD_NOT_ALLOWED.");
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
     }
 
@@ -226,6 +276,7 @@ public class FileController {
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl("no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
+        logger.error("Head /user/self/pic called, returning METHOD_NOT_ALLOWED.");
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
     }
     @RequestMapping(method = RequestMethod.OPTIONS)
@@ -233,6 +284,7 @@ public class FileController {
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl("no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
+        logger.error("Options /user/self/pic called, returning METHOD_NOT_ALLOWED.");
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
     }
 
