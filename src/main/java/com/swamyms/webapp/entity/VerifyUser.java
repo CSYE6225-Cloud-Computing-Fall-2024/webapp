cf6d5e4143e51d9fbc2722970bfbc92f9e4d5905
swamyms <mudiga.s@northeastern.edu>
2024-11-14 01:00:23 -0500

    Verifyuser entity added

---
+++ src/main/java/com/swamyms/webapp/entity/VerifyUser.java
@@ -0,0 +1,46 @@
+package com.swamyms.webapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Setter
+@Getter
+@Entity(name = "verify_user")
+@Table(name="verify_user")
+@Component
+public class VerifyUser {
+    @Id
+    @UuidGenerator
+    private String id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "verified")
+    private boolean verified;
+
+    @Column(name = "email_sent")
+    @CreationTimestamp
+    private Instant emailSent;
+
+    @Column(name = "verify_email_sent")
+    private boolean verifyEmailSent;
+
+    public VerifyUser() {}
+
+    public VerifyUser(String username) {
+        this.email = username;
+        this.verified = false;
+        this.verifyEmailSent = false;
+    }
+
+}
