79f00685b74b75d209974b62160a9113625db1b6
swamyms <mudiga.s@northeastern.edu>
2024-11-21 00:34:58 -0500

    Copied /healthz endpoints method to make new /cicd enpoint to test as per Assignment 08 requirement

--- src/main/java/com/swamyms/webapp/controllers/HealthzRestController.java
+++ src/main/java/com/swamyms/webapp/controllers/HealthzRestController.java
@@ -18,7 +18,6 @@ import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 @RestController
-@RequestMapping("/healthz")
 
 public class HealthzRestController {
 
@@ -47,7 +46,7 @@ public class HealthzRestController {
     private final Timer apiCallTimer;
     private final Timer dbQueryTimer;
 
-    @GetMapping
+    @GetMapping("/healthz")
     private ResponseEntity<?> getHealthzStatus(@RequestParam(required = false) HashMap<String, String> params, // Check for query parameters
                                                @RequestBody(required = false) String requestBody // Check for request body
     ) {
@@ -88,7 +87,7 @@ public class HealthzRestController {
             return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).headers(headers).build();
         }
     }
-    @PostMapping
+    @PostMapping("/healthz")
     private ResponseEntity<String> handlePostHealthzStatus(){
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl("no-cache, no-store, must-revalidate");
@@ -98,7 +97,7 @@ public class HealthzRestController {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
     }
 
-    @DeleteMapping
+    @DeleteMapping("/healthz")
     private ResponseEntity<String> handleDeleteHealthzStatus(){
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl("no-cache, no-store, must-revalidate");
@@ -107,7 +106,7 @@ public class HealthzRestController {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
 
     }
-    @PutMapping
+    @PutMapping("/healthz")
     private ResponseEntity<String> handlePutHealthzStatus(){
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl("no-cache, no-store, must-revalidate");
@@ -116,7 +115,7 @@ public class HealthzRestController {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
     }
 
-    @PatchMapping
+    @PatchMapping("/healthz")
     private ResponseEntity<String> handlePatchHealthzStatus(){
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl("no-cache, no-store, must-revalidate");
@@ -125,7 +124,7 @@ public class HealthzRestController {
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
     }
 
-    @RequestMapping(method = RequestMethod.HEAD)
+    @RequestMapping(value = "/healthz",method = RequestMethod.HEAD)
     private ResponseEntity<String>  handleHeadHealthzStatus(){
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl("no-cache, no-store, must-revalidate");
@@ -133,8 +132,103 @@ public class HealthzRestController {
         logger.error("HEAD /healthz called, returning METHOD_NOT_ALLOWED.");
         return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
     }
-    @RequestMapping(method = RequestMethod.OPTIONS)
+    @RequestMapping(value = "/healthz",method = RequestMethod.OPTIONS)
     private ResponseEntity<String>  handleOptionsHealthzStatus(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl("no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        logger.error("OPTIONS /healthz called, returning METHOD_NOT_ALLOWED.");
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
+    }
+
+    @GetMapping("/cicd")
+    private ResponseEntity<?> getCICDStatus(@RequestParam(required = false) HashMap<String, String> params, // Check for query parameters
+                                               @RequestBody(required = false) String requestBody // Check for request body
+    ) {
+        // Prepare headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl("no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+
+        // Check if there are any query parameters or a request body
+        if ((params != null && !params.isEmpty()) || (requestBody != null && !requestBody.isEmpty())) {
+            logger.warn("Bad request: unexpected parameters or body present"); // Log warning
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).headers(headers).build();
+        }
+        long startTime = System.currentTimeMillis(); // Start timing API call
+        try {
+            Timer.Sample sample = Timer.start(meterRegistry); // Start Timer for the API call
+
+            // Execute a simple query to check the health of the database
+            long dbStartTime = System.currentTimeMillis(); // Start timing DB query
+            Query query = entityManager.createNativeQuery("SELECT 1");
+            query.getSingleResult();
+
+            long dbEndTime = System.currentTimeMillis(); // End timing DB query
+
+            // Record the time taken for the database query
+            dbQueryTimer.record(dbEndTime - dbStartTime, TimeUnit.MILLISECONDS);
+
+            // Record the API call duration
+            sample.stop(apiCallTimer);
+
+            logger.info("Health check successful. Time taken: {} ms", System.currentTimeMillis() - startTime); // Log info
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .headers(headers).build();
+        }catch (PersistenceException pe){
+            logger.error("Database connection error: {}", pe.getMessage()); // Log error
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).headers(headers).build();
+        }
+    }
+    @PostMapping("/cicd")
+    private ResponseEntity<String> handlePostCICDStatus(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl("no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        // Log the method call
+        logger.error("POST /healthz called, returning METHOD_NOT_ALLOWED.");
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
+    }
+
+    @DeleteMapping("/cicd")
+    private ResponseEntity<String> handleDeleteCICDStatus(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl("no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        logger.error("Delete /healthz called, returning METHOD_NOT_ALLOWED.");
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
+
+    }
+    @PutMapping("/cicd")
+    private ResponseEntity<String> handlePutCICDStatus(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl("no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        logger.error("Put /healthz called, returning METHOD_NOT_ALLOWED.");
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
+    }
+
+    @PatchMapping("/cicd")
+    private ResponseEntity<String> handlePatchCICDStatus(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl("no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        logger.error("Patch /healthz called, returning METHOD_NOT_ALLOWED.");
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
+    }
+
+    @RequestMapping(value = "/cicd",method = RequestMethod.HEAD)
+    private ResponseEntity<String>  handleHeadCICDStatus(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl("no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        logger.error("HEAD /healthz called, returning METHOD_NOT_ALLOWED.");
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).headers(headers).build();
+    }
+    @RequestMapping(value = "/cicd",method = RequestMethod.OPTIONS)
+    private ResponseEntity<String>  handleOptionsCICDStatus(){
         HttpHeaders headers = new HttpHeaders();
         headers.setCacheControl("no-cache, no-store, must-revalidate");
         headers.add("Pragma", "no-cache");
